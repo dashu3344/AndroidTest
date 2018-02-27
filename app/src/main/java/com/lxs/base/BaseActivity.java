115b6ae42c10cdf41788730c54ad11c69da60518
lxs <dashu3344@163.com>
2018-02-27 14:46:34 +0800

    Initial commit

---
+++ app/src/main/java/com/lxs/base/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.lxs.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author   :  lxs .
+ * E-mail   :  dashu3344@gmail.com
+ * Time     :  2018/02/27/10
+ * Desc     :
+ * Version  :  1.0
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/lxs/data_storage/SharedPreferencesActivity.java
@@ -0,0 +1,15 @@
+package com.lxs.data_storage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lxs.androidtest.R;
+
+public class SharedPreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_preferences);
+    }
+}
